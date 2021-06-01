c959a39570142901b865aa14a46773741580722e
ivnicolas <ikennanicolas@gmail.com>
2021-05-31 23:18:05 -0400

    commit tree 0e5a7675a154f0ba72aabd31b896b662f1444b76

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+//the following gives the right solution but not what the test wanted...
+// function totalBatteries(){
+//     let batteries = batteryBatches.reduce(function(total,element){return element + total},0)
+//     return batteries
+// }
+// what the test wanted.....
+const totalBatteries = batteryBatches.reduce(function(total, batches){
+    return batches + total
+    }
+  )
